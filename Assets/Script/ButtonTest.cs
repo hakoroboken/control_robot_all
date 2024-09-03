906a62d3b162cbadb9ab40407e2505b270885168
A-KoiKING <yukisnowskor@gmail.com>
2024-09-03 15:29:53 +0900

    NEW UI

---
+++ Assets/Script/ButtonTest.cs
@@ -0,0 +1,295 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    private GameInputs _gameInputs;
+
+    public GameObject _circle;
+    public GameObject _cross;
+    public GameObject _square;
+    public GameObject _triangle;
+    public GameObject _up;
+    public GameObject _down;
+    public GameObject _left;
+    public GameObject _right;
+    public GameObject _r1;
+    public GameObject _r2;
+    public GameObject _r3;
+    public GameObject _l1;
+    public GameObject _l2;
+    public GameObject _l3;
+    public GameObject _option;
+    public GameObject _share;
+
+    public GameObject _R;
+    public GameObject _L;
+
+    private Image circle;
+    private Image cross;
+    private Image square;
+    private Image triangle;
+    private Image up;
+    private Image down;
+    private Image right;
+    private Image left;
+    private Image r1;
+    private Image r2;
+    private Image r3;
+    private Image l1;
+    private Image l2;
+    private Image l3;
+    private Image option;
+    private Image share;
+
+    public Sprite[] _arrow;
+    public Sprite[] _button;
+    public Sprite[] _1;
+    public Sprite[] _2;
+    public Sprite[] _minibutton;
+
+    [SerializeField] private Vector2 _LeftInputValue;
+    [SerializeField] private Vector2 _RightInputValue;
+
+    private void Awake()
+    {
+        // Actionスクリプトのインスタンス生成
+        _gameInputs = new GameInputs();
+
+        _gameInputs.ButtonTest.Circle.performed += OnCircleStart;
+        _gameInputs.ButtonTest.Circle.canceled += OnCircleEnd;
+        _gameInputs.ButtonTest.Cross.performed += OnCrossStart;
+        _gameInputs.ButtonTest.Cross.canceled += OnCrossEnd;
+        _gameInputs.ButtonTest.Square.performed += OnSquareStart;
+        _gameInputs.ButtonTest.Square.canceled += OnSquareEnd;
+        _gameInputs.ButtonTest.Triangle.performed += OnTriangleStart;
+        _gameInputs.ButtonTest.Triangle.canceled += OnTriangleEnd;
+
+        _gameInputs.ButtonTest.Up.performed += OnUpStart;
+        _gameInputs.ButtonTest.Up.canceled += OnUpEnd;
+        _gameInputs.ButtonTest.Down.performed += OnDownStart;
+        _gameInputs.ButtonTest.Down.canceled += OnDownEnd;
+        _gameInputs.ButtonTest.Right.performed += OnRightStart;
+        _gameInputs.ButtonTest.Right.canceled += OnRightEnd;
+        _gameInputs.ButtonTest.Left.performed += OnLeftStart;
+        _gameInputs.ButtonTest.Left.canceled += OnLeftEnd;
+
+        _gameInputs.ButtonTest.R1.performed += OnR1Start;
+        _gameInputs.ButtonTest.R1.canceled += OnR1End;
+        _gameInputs.ButtonTest.R2.performed += OnR2Start;
+        _gameInputs.ButtonTest.R2.canceled += OnR2End;
+        _gameInputs.ButtonTest.R3.performed += OnR3Start;
+        _gameInputs.ButtonTest.R3.canceled += OnR3End;
+        _gameInputs.ButtonTest.L1.performed += OnL1Start;
+        _gameInputs.ButtonTest.L1.canceled += OnL1End;
+        _gameInputs.ButtonTest.L2.performed += OnL2Start;
+        _gameInputs.ButtonTest.L2.canceled += OnL2End;
+        _gameInputs.ButtonTest.L3.performed += OnL3Start;
+        _gameInputs.ButtonTest.L3.canceled += OnL3End;
+
+        _gameInputs.ButtonTest.Options.performed += OnOptionStart;
+        _gameInputs.ButtonTest.Options.canceled += OnOptionEnd;
+        _gameInputs.ButtonTest.Share.performed += OnShareStart;
+        _gameInputs.ButtonTest.Share.canceled += OnShareEnd;
+
+        _gameInputs.ButtonTest.RightStick.started += OnRight;
+        _gameInputs.ButtonTest.RightStick.performed += OnRight;
+        _gameInputs.ButtonTest.RightStick.canceled += OnRight;
+        _gameInputs.ButtonTest.LeftStick.started += OnLeft;
+        _gameInputs.ButtonTest.LeftStick.performed += OnLeft;
+        _gameInputs.ButtonTest.LeftStick.canceled += OnLeft;
+
+        // Input Actionを機能させるためには、
+        // 有効化する必要がある
+        _gameInputs.Enable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        circle = _circle.GetComponent<Image>();
+        cross = _cross.GetComponent<Image>();
+        square = _square.GetComponent<Image>();
+        triangle = _triangle.GetComponent<Image>();
+        up = _up.GetComponent<Image>();
+        down = _down.GetComponent<Image>();
+        right = _right.GetComponent<Image>();
+        left = _left.GetComponent<Image>();
+        r1 = _r1.GetComponent<Image>();
+        r2 = _r2.GetComponent<Image>();
+        r3 = _r3.GetComponent<Image>();
+        l1 = _l1.GetComponent<Image>();
+        l2 = _l2.GetComponent<Image>();
+        l3 = _l3.GetComponent<Image>();
+        option = _option.GetComponent<Image>();
+        share = _share.GetComponent<Image>();
+}
+
+    private void OnDestroy()
+    {
+        // 自身でインスタンス化したActionクラスはIDisposableを実装しているので、
+        // 必ずDisposeする必要がある
+        _gameInputs?.Dispose();
+    }
+
+    private void OnCircleStart(InputAction.CallbackContext context)
+    {
+        circle.sprite = _button[1];
+    }
+    private void OnCircleEnd(InputAction.CallbackContext context)
+    {
+        circle.sprite = _button[0];
+    }
+    private void OnCrossStart(InputAction.CallbackContext context)
+    {
+        cross.sprite = _button[1];
+    }
+    private void OnCrossEnd(InputAction.CallbackContext context)
+    {
+        cross.sprite = _button[0];
+    }
+    private void OnSquareStart(InputAction.CallbackContext context)
+    {
+        square.sprite = _button[1];
+    }
+    private void OnSquareEnd(InputAction.CallbackContext context)
+    {
+        square.sprite = _button[0];
+    }
+    private void OnTriangleStart(InputAction.CallbackContext context)
+    {
+        triangle.sprite = _button[1];
+    }
+    private void OnTriangleEnd(InputAction.CallbackContext context)
+    {
+        triangle.sprite = _button[0];
+    }
+    private void OnUpStart(InputAction.CallbackContext context)
+    {
+        up.sprite = _arrow[1];
+    }
+    private void OnUpEnd(InputAction.CallbackContext context)
+    {
+        up.sprite = _arrow[0];
+    }
+    private void OnDownStart(InputAction.CallbackContext context)
+    {
+        down.sprite = _arrow[1];
+    }
+    private void OnDownEnd(InputAction.CallbackContext context)
+    {
+        down.sprite = _arrow[0];
+    }
+    private void OnRightStart(InputAction.CallbackContext context)
+    {
+        right.sprite = _arrow[1];
+    }
+    private void OnRightEnd(InputAction.CallbackContext context)
+    {
+        right.sprite = _arrow[0];
+    }
+    private void OnLeftStart(InputAction.CallbackContext context)
+    {
+        left.sprite = _arrow[1];
+    }
+    private void OnLeftEnd(InputAction.CallbackContext context)
+    {
+        left.sprite = _arrow[0];
+    }
+    private void OnR1Start(InputAction.CallbackContext context)
+    {
+        r1.sprite = _1[1];
+    }
+    private void OnR1End(InputAction.CallbackContext context)
+    {
+        r1.sprite = _1[0];
+    }
+    private void OnR2Start(InputAction.CallbackContext context)
+    {
+        r2.sprite = _2[1];
+    }
+    private void OnR2End(InputAction.CallbackContext context)
+    {
+        r2.sprite = _2[0];
+    }
+    private void OnR3Start(InputAction.CallbackContext context)
+    {
+        r3.sprite = _button[1];
+    }
+    private void OnR3End(InputAction.CallbackContext context)
+    {
+        r3.sprite = _button[0];
+    }
+    private void OnL1Start(InputAction.CallbackContext context)
+    {
+        l1.sprite = _1[1];
+    }
+    private void OnL1End(InputAction.CallbackContext context)
+    {
+        l1.sprite = _1[0];
+    }
+    private void OnL2Start(InputAction.CallbackContext context)
+    {
+        l2.sprite = _2[1];
+    }
+    private void OnL2End(InputAction.CallbackContext context)
+    {
+        l2.sprite = _2[0];
+    }
+    private void OnL3Start(InputAction.CallbackContext context)
+    {
+        l3.sprite = _button[1];
+    }
+    private void OnL3End(InputAction.CallbackContext context)
+    {
+        l3.sprite = _button[0];
+    }
+    private void OnOptionStart(InputAction.CallbackContext context)
+    {
+        option.sprite = _minibutton[1];
+    }
+    private void OnOptionEnd(InputAction.CallbackContext context)
+    {
+        option.sprite = _minibutton[0];
+    }
+    private void OnShareStart(InputAction.CallbackContext context)
+    {
+        share.sprite = _minibutton[1];
+    }
+    private void OnShareEnd(InputAction.CallbackContext context)
+    {
+        share.sprite = _minibutton[0];
+    }
+    private void OnRight(InputAction.CallbackContext context)
+    {
+        _RightInputValue = context.ReadValue<Vector2>();
+        _RightInputValue *= 5.5f;
+        _R.transform.localPosition = _RightInputValue;
+    }
+    private void OnLeft(InputAction.CallbackContext context)
+    {
+        _LeftInputValue = context.ReadValue<Vector2>();
+        _LeftInputValue *= 5.5f;
+        _L.transform.localPosition = _LeftInputValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 接続されているコントローラの名前を調べる
+        var controllerNames = Input.GetJoystickNames();
+
+        // 一台もコントローラが接続されていなければエラー
+        if (controllerNames[0] == "") {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Script/UdpSend.cs
+++ Assets/Script/UdpSend.cs
@@ -3,24 +3,41 @@ using System.Net.Sockets;
 using System.Text;
 using TMPro;
 using UnityEngine.UI;
+using Palmmedia.ReportGenerator.Core.Common;
 
 public class UdpSend : MonoBehaviour
 {
-    public string host = "172.20.10.2";
-    public int port = 9000;
+    private string host;
+    private int IP1;
+    private int IP2;
+    private int IP3;
+    private int IP4;
+    private int port = 62455;
     private UdpClient client;
 
     //オブジェクトと結びつける
     public TextMeshProUGUI IPNumber;
     public TextMeshProUGUI PortNumber;
     public TextMeshProUGUI SendingMessage;
-    public TMP_InputField IP_InputField;
+    public TMP_InputField IP_1_InputField;
+    public TMP_InputField IP_2_InputField;
+    public TMP_InputField IP_3_InputField;
+    public TMP_InputField IP_4_InputField;
     public TMP_InputField Port_InputField;
-    public PlayerMover _playerMover;
+    public CreateMessage _CreateMessage;
+    public StopButton _StopButton;
 
     void Start()
     {
-        IP_InputField.text = host;
+        IP1 = 192;
+        IP2 = 168;
+        IP3 = 10;
+        IP4 = 123;
+        host = IP1 + "." + IP2 + "." + IP3 +"." + IP4;
+        IP_1_InputField.text = IP1.ToString();
+        IP_2_InputField.text = IP2.ToString();
+        IP_3_InputField.text = IP3.ToString();
+        IP_4_InputField.text = IP4.ToString();
         IPNumber.text = host;
         Port_InputField.text = port.ToString();
         PortNumber.text = port.ToString();
@@ -32,9 +49,13 @@ public class UdpSend : MonoBehaviour
 
     void Update()
     {
-        SendingMessage.text = _playerMover.log;
-        var message = Encoding.UTF8.GetBytes(_playerMover.log);
-        Debug.Log(_playerMover.log);
+        SendingMessage.text = _CreateMessage.log;
+        if (_StopButton.stop == true)
+        {
+            _CreateMessage.log = "s,0.500,0.500,0.500,0,0,0,e";
+        }
+        var message = Encoding.UTF8.GetBytes(_CreateMessage.log);
+        Debug.Log(_CreateMessage.log);
         client.Send(message, message.Length);
     }
 
@@ -45,9 +66,16 @@ public class UdpSend : MonoBehaviour
 
     public void SetText()
     {
-        host = IP_InputField.text;
+        IP1 = int.Parse(IP_1_InputField.text);
+        IP2 = int.Parse(IP_2_InputField.text);
+        IP3 = int.Parse(IP_3_InputField.text);
+        IP4 = int.Parse(IP_4_InputField.text);
+        host = IP1 + "." + IP2 + "." + IP3 + "." + IP4;
         IPNumber.text = host;
         port = int.Parse(Port_InputField.text);
         PortNumber.text = port.ToString();
+
+        client = new UdpClient();
+        client.Connect(host, port);
     }
 }
