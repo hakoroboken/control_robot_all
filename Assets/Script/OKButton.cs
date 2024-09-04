b69b9375866101277ca4c39d0c19bd8cbbe2ed5c
A-KoiKING <yukisnowskor@gmail.com>
2024-09-04 18:19:34 +0900

    Fine adjustments

--- Assets/Script/OKButton.cs
+++ Assets/Script/OKButton.cs
@@ -2,15 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class OKButton : MonoBehaviour
 {
+    public int okcount;
     public StopButton _StopButton;
     public TextMeshProUGUI _StopText;
+    public GameObject _greencircle;
+    public GameObject _redcircle;
+    private Vector3 okscale = new Vector3(1, 1, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _greencircle.transform.localScale = new Vector3(1, 1, 1);
+        okcount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (okcount >= 1)
+        {
+            _greencircle.transform.localScale = okscale * okcount * 0.5f;
+            okcount++;
+            if (okcount >= 400)
+            {
+                okcount = 0;
+                _redcircle.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
 
     public void Onclick()
     {
-        _StopButton.stop = false;
-        _StopText.text = "";
+        if(_StopButton.stop == true)
+        {
+            _StopText.text = "";
+            okcount = 1;
+            _StopButton.stop = false;
+            _StopButton._backimage.sprite = _StopButton._back[1];
+            _greencircle.transform.SetSiblingIndex(2);
+        }
     }
 }
